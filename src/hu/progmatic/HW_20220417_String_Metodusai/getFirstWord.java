bc9ccfbed0b84805a2707e09465c5ad625b5d309
Peter Szabo <szwazzup@gmail.com>
2022-04-18 22:01:51 +0200

    String Metodusok

---
+++ src/hu/progmatic/HW_20220417_String_Metodusai/getFirstWord.java
@@ -0,0 +1,22 @@
+package hu.progmatic.HW_20220417_String_Metodusai;
+
+import java.util.Scanner;
+
+public class getFirstWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem, adjon meg egy szöveget - visszaadom az első szavát.");
+        String pharase = sc.nextLine();
+
+        System.out.println(getFirstWord(pharase));
+    }
+
+    public static String getFirstWord(String text) {
+        int index = text.indexOf(' ');
+        if (index > -1) {
+            return text.substring(0, index);
+        } else {
+            return text;
+        }
+    }
+}
---
+++ src/hu/progmatic/HW_20220417_String_Metodusai/getLastWord.java
@@ -0,0 +1,20 @@
+package hu.progmatic.HW_20220417_String_Metodusai;
+
+import java.util.Scanner;
+
+public class getLastWord {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem, adjon meg egy szöveget - visszaadom az utolsó szavát.");
+        String word = sc.nextLine();
+
+        System.out.println(getLastWord(word));
+    }
+
+    public static String getLastWord(String text) {
+        int lastIndex = text.lastIndexOf(" ");
+        String word = text.substring(lastIndex + 1);
+
+        return word;
+    }
+}
